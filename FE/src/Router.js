fe1f8ea6bc99f7fa59716cb4b85724035cbae33e
정혜진 <pado7sea@gmail.com>
2024-01-28 00:28:55 +0900

    Feat: 기본 페이지 연결 경로

--- FE/src/Router.js
+++ FE/src/Router.js
@@ -1,8 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import App from "./components/App";
 import MainPage from "./pages/MainPage";
-import ScrollToTop from "./components/ScrollToTop";
-import LearningPage from "./pages/LearningPage";
+import QuizLobbyPage from "./pages/QuizLobbyPage";
+import SingleplayPage from "./pages/SingleplayPage";
+import WaitingPage from "./pages/WaitingPage";
+import MultiplayPage from "./pages/MultiplayPage";
+import SelectPage from "./pages/SelectPage";
+import BookmarkPage from "./pages/BookmarkPage";
+import LearningStartPage from "./pages/LearningStartPage";
 
 function Router() {
   return (
@@ -11,8 +17,18 @@ function Router() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<MainPage />} />
+          <Route path="quizLobby" element={<QuizLobbyPage />} />
+          <Route path="singleplay">
+            <Route index element={<SingleplayPage />} />
+          </Route>
+          <Route path="multiplay">
+            <Route index element={<WaitingPage />} />
+            <Route path="start" element={<MultiplayPage />} />
+          </Route>
           <Route path="learning">
-            <Route index element={<LearningPage />} />
+            <Route index element={<SelectPage />} />
+            <Route path="bookmark" element={<BookmarkPage />} />
+            <Route path="start" element={<LearningStartPage />} />
           </Route>
         </Route>
       </Routes>
--- FE/src/pages/LearningStartPage.js
+++ FE/src/pages/LearningStartPage.js
@@ -1,13 +1,10 @@
 import Container from "../components/Container";
-import SelectCategory from "../feature/Learning/SelectCategory";
 
 export default function LearningPage() {
   return (
     <>
       <Container>
         <div>학습하기페이지입니다.</div>
-
-        <SelectCategory />
       </Container>
     </>
   );
