b4c8e303f838c96284dbe3037aec6bb2e150bcfa
정혜진 <pado7sea@gmail.com>
2024-01-27 23:35:32 +0900

    Refactor: 컴포넌트화

--- FE/src/feature/Learning/SelectCategory.js
+++ FE/src/feature/Learning/SelectCategory.js
@@ -1,47 +1,47 @@
 import React, { useState } from "react";
 
-function SelectCategory() {
-  const [selectedArea, setSelectedArea] = useState("");
-  const [selectedSubArea, setSelectedSubArea] = useState("");
+function SelectCategory({ startLearning }) {
+  const [selectedMain, setselectedMain] = useState("");
+  const [selectedSub, setSelectedSub] = useState("");
   const [start, setStart] = useState(false);
 
   const mainCategories = ["자음", "모음", "숫자", "낱말", "단어장"];
   const subCategories = ["동물", "사물", "일상"];
 
-  const handleMainCategorySelect = (category) => {
-    setSelectedArea(category);
+  const handleMain = (category) => {
+    setselectedMain(category);
     setStart(category !== "낱말");
-    if (category !== "낱말") setSelectedSubArea("");
+    if (category !== "낱말") setSelectedSub("");
   };
 
-  const handleSubCategorySelect = (subCategory) => {
-    setSelectedSubArea(subCategory);
+  const handleSub = (subCategory) => {
+    setSelectedSub(subCategory);
     setStart(true);
   };
 
-  return (
-    <div>
-      {/* 기본 카테고리 선택 버튼들 */}
-      {mainCategories.map((category) => (
-        <button key={category} onClick={() => handleMainCategorySelect(category)}>
+  const CategoryButtons = ({ categories, onClick }) => (
+    <>
+      {categories.map((category) => (
+        <button key={category} onClick={() => onClick(category)}>
           {category}
         </button>
       ))}
+    </>
+  );
 
-      {/* '낱말' 선택 시 추가적인 선택지 */}
-      {selectedArea === "낱말" &&
-        subCategories.map((subCategory) => (
-          <button key={subCategory} onClick={() => handleSubCategorySelect(subCategory)}>
-            {subCategory}
-          </button>
-        ))}
-
-      {/* 시작하기 부분에 선택한 카테고리와 서브카테고리 표시 */}
+  return (
+    <div>
+      <CategoryButtons categories={mainCategories} onClick={handleMain} />
+      {selectedMain === "낱말" && (
+        <div>
+          <CategoryButtons categories={subCategories} onClick={handleSub} />
+        </div>
+      )}
       {start && (
         <div>
-          선택한 카테고리: {selectedArea}
-          {selectedArea === "낱말" && <div>선택한 서브 카테고리: {selectedSubArea}</div>}
-          <div>시작하기</div>
+          <div>선택한 카테고리: {selectedMain}</div>
+          {selectedMain === "낱말" && <div>선택한 서브 카테고리: {selectedSub}</div>}
+          <button onClick={startLearning}>시작하기</button>
         </div>
       )}
     </div>
--- FE/src/pages/LearningPage.js
+++ FE/src/pages/LearningPage.js
@@ -5,7 +5,8 @@ export default function LearningPage() {
   return (
     <>
       <Container>
-        <div>학습하기</div>
+        <div>학습하기페이지입니다.</div>
+
         <SelectCategory />
       </Container>
     </>
