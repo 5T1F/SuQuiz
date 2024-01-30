58fe75e045fa6ee1a72e09a3d9f26518ae9f5c97
정혜진 <pado7sea@gmail.com>
2024-01-30 20:58:08 +0900

    Fix: 학습 분야 선택 페이지 CSS

--- FE/src/feature/Learning/SelectCategory.js
+++ FE/src/feature/Learning/SelectCategory.js
@@ -2,55 +2,63 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function SelectCategory() {
-  const [selectedMain, setselectedMain] = useState("");
+  const [selectedMain, setSelectedMain] = useState("");
   const [selectedSub, setSelectedSub] = useState("");
-  const [start, setStart] = useState(false);
-
   const navigate = useNavigate();
 
   const mainCategories = ["자음", "모음", "숫자", "낱말", "단어장"];
   const subCategories = ["동물", "사물", "일상"];
 
-  const handleMain = (category) => {
-    setselectedMain(category);
-    setStart(category !== "낱말");
-    if (category !== "낱말") setSelectedSub("");
+  const handleMainCategoryChange = (category) => {
+    setSelectedMain(category);
+    if (category !== "낱말") {
+      setSelectedSub("");
+    }
   };
 
-  const handleSub = (subCategory) => {
+  const handleSubCategoryChange = (subCategory) => {
     setSelectedSub(subCategory);
-    setStart(true);
   };
 
   const handleStart = () => {
-    navigate("/learning/start", { state: { selectedMain, selectedSub } }); // v6에서 사용하는 경우
+    navigate("/learning/start", { state: { selectedMain, selectedSub } });
   };
 
-  const CategoryButtons = ({ categories, onClick }) => (
-    <>
-      {categories.map((category) => (
-        <button key={category} onClick={() => onClick(category)}>
-          {category}
-        </button>
-      ))}
-    </>
+  const CategoryButton = ({ category, onClick }) => (
+    <button
+      className="w-16 h-9 p-5 bg-white rounded-lg shadow border-2 border-amber-200 flex justify-center items-center"
+      onClick={() => onClick(category)}
+    >
+      <div className="text-center text-yellow-950 text-base">{category}</div>
+    </button>
   );
 
   return (
-    <>
-      <CategoryButtons categories={mainCategories} onClick={handleMain} />
+    <div className="w-full max-w-4xl h-[90vh] flex flex-col justify-start items-center gap-8">
+      <div className="text-yellow-950 text-base font-black">오늘의 학습 분야를 선택하세요</div>
+      <div className="flex justify-center items-center gap-6">
+        {mainCategories.map((category) => (
+          <CategoryButton key={category} category={category} onClick={handleMainCategoryChange} />
+        ))}
+      </div>
       {selectedMain === "낱말" && (
-        <div>
-          <CategoryButtons categories={subCategories} onClick={handleSub} />
+        <div className="flex flex-col justify-start items-center gap-6">
+          <div className="text-yellow-950 text-base font-black">낱말 주제를 고르세요</div>
+          <div className="flex justify-center items-center gap-6">
+            {subCategories.map((subCategory) => (
+              <CategoryButton key={subCategory} category={subCategory} onClick={handleSubCategoryChange} />
+            ))}
+          </div>
         </div>
       )}
-      {start && (
-        <div>
-          <div>선택한 카테고리: {selectedMain}</div>
-          {selectedMain === "낱말" && <div>선택한 서브 카테고리: {selectedSub}</div>}
-          <button onClick={handleStart}>시작하기</button>
-        </div>
+      {((selectedMain && selectedMain !== "낱말") || (selectedMain === "낱말" && selectedSub)) && (
+        <button
+          onClick={handleStart}
+          className="px-6 py-2 bg-amber-200  hover:bg-amber-400 rounded-lg shadow border-2 border-amber-200 flex justify-center items-center transition duration-300"
+        >
+          <div className="text-center text-yellow-950 text-base font-black">시작하기</div>
+        </button>
       )}
-    </>
+    </div>
   );
 }
