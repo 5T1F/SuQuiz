079ae91cdc47891c4442c756e4a5728cca0a9dc8
정혜진 <pado7sea@gmail.com>
2024-01-30 09:08:13 +0900

    Fix: 사이드 메뉴 단어 여러번 나오는 에러 수정

--- FE/src/feature/Learning/Tab.js
+++ FE/src/feature/Learning/Tab.js
@@ -18,33 +18,18 @@ function CustomTab({ selectedMain, selectedSub, wordList, setCurrentWord }) {
   return (
     <Tabs value={activeTab} onChange={(value) => setActiveTab(value)}>
       <TabsHeader>
-        {mainCategories.map(
-          (
-            category,
-            index // index 변수 추가
-          ) => (
-            <Tab key={index} value={category}>
-              <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-                {category}
-              </div>
-            </Tab>
-          )
-        )}
+        {mainCategories.map((category) => (
+          <Tab key={category} value={category}>
+            <div>{category}</div>
+          </Tab>
+        ))}
       </TabsHeader>
       <TabsBody>
-        {wordList.map(
-          (
-            word,
-            index // index 변수 추가, word 변수 사용
-          ) => (
-            <TabPanel key={index} value={word.category}>
-              <WordList
-                wordsProp={wordList.filter((w) => w.category === word.category)}
-                setCurrentWord={setCurrentWord}
-              />
-            </TabPanel>
-          )
-        )}
+        {mainCategories.map((category) => (
+          <TabPanel key={category} value={category}>
+            <WordList wordsProp={wordList.filter((w) => w.category === category)} setCurrentWord={setCurrentWord} />
+          </TabPanel>
+        ))}
       </TabsBody>
     </Tabs>
   );
