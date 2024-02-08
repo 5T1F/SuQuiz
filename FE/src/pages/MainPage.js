6b84a0e06aaed817e85052effaef2cf2f0a50b49
정혜진 <pado7sea@gmail.com>
2024-02-08 20:28:32 +0900

    Refactor:  link 대신 useNavigate 쓰도록 바꿈

--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Container from "../components/Container";
 
@@ -7,15 +7,25 @@ import styles from "./MainPage.module.css";
 import backgroundVideo from "../assets/backgroundVideo.mp4";
 
 export default function MainPage() {
+  const navigate = useNavigate();
+
+  const handleQuizStart = () => {
+    navigate("/quizLobby");
+  };
+
+  const handleLearningStart = () => {
+    navigate("/learning");
+  };
+
   return (
     <>
       <Container>
         <div className={styles.container}>
           <div className={styles.startbuttons}>
-            <button className={styles.quizStart} onClick={() => <Navigate to="/quizLobby" />}>
+            <button className={styles.quizStart} onClick={handleQuizStart}>
               퀴즈 시작
             </button>
-            <button className={styles.learningStart} onClick={() => <Navigate to="/learning" />}>
+            <button className={styles.learningStart} onClick={handleLearningStart}>
               학습 시작
             </button>
           </div>
