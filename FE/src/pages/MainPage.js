3ebf55c74e829fa5849a6356c11e091b0c6fe116
정혜진 <pado7sea@gmail.com>
2024-02-08 17:42:33 +0900

    Feat: 메인페이지 CSS

--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,24 +1,29 @@
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 import Container from "../components/Container";
 
 import styles from "./MainPage.module.css";
 
+import backgroundVideo from "../assets/backgroundVideo.mp4";
+
 export default function MainPage() {
   return (
     <>
       <Container>
-        <div className={styles.quizStart}>
-          <Link to="/quizLobby">
-            <button>퀴즈 시작</button>
-          </Link>
-        </div>
-        <div className={styles.learningStart}>
-          <Link to="/learning">
-            <button>학습 시작</button>
-          </Link>
+        <div className={styles.container}>
+          <div className={styles.startbuttons}>
+            <button className={styles.quizStart} onClick={() => <Navigate to="/quizLobby" />}>
+              퀴즈 시작
+            </button>
+            <button className={styles.learningStart} onClick={() => <Navigate to="/learning" />}>
+              학습 시작
+            </button>
+          </div>
         </div>
       </Container>
+      <video autoPlay loop muted>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
     </>
   );
 }
