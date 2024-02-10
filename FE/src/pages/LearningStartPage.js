b7b849c8d103744a16801f6efa2fe6ac1eeaa572
김현준 <denny10002@naver.com>
2024-02-10 14:00:07 +0900

    Feat : single learn css

--- FE/src/pages/LearningStartPage.js
+++ FE/src/pages/LearningStartPage.js
@@ -8,71 +8,72 @@ import { wordsfromCategory, AllWordWithSubject } from "../apis/learningApi";
 import MyCam from "../feature/Learning/MyCam";
 
 export default function LearningStartPage() {
-    const location = useLocation();
-    const { selectedMain, selectedSub } = location.state || {
-        selectedMain: null,
-        selectedSub: null,
+  const [isHovered, setIsHovered] = useState(false);
+  const location = useLocation();
+  const { selectedMain, selectedSub } = location.state || {
+    selectedMain: null,
+    selectedSub: null,
+  };
+  const [currentWord, setCurrentWord] = useState(null);
+  const [wordList, setWordList] = useState([]);
+
+  // motion detect value
+  const [finger, setFinger] = useState("#");
+  const changeFinger = (value) => {
+    setFinger(value);
+  };
+
+  // const []
+
+  console.log(selectedMain, selectedSub);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await wordsfromCategory(1, selectedMain); //userId 대신에  테스트용 1
+        setWordList(data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    const [currentWord, setCurrentWord] = useState(null);
-    const [wordList, setWordList] = useState([]);
 
-    // motion detect value
-    const [finger, setFinger] = useState("#");
-    const changeFinger = (value) => {
-        setFinger(value);
-    };
+    fetchData();
+  }, [selectedMain, selectedSub]);
 
-    // const []
-
-    console.log(selectedMain, selectedSub);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await wordsfromCategory(1, selectedMain); //userId 대신에  테스트용 1
-                setWordList(data.data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-    }, [selectedMain, selectedSub]);
-
-    return (
-        <Container>
-            <h1 className="text-3xl font-bold underline">학습 시작 페이지</h1>
-            <div className="flex">
-                {/* 사이드메뉴 */}
-                <div className="w-[270px] h-[90vh] p-1 border-4 border-red-500">
-                    <SideMenu
-                        selectedMain={selectedMain}
-                        selectedSub={selectedSub}
-                        wordList={wordList}
-                        setCurrentWord={setCurrentWord}
-                    />
-                </div>
-                {/* 학습 화면 */}
-                <div className="px-40 border-4 border-violet-500 w-full">
-                    {/* 플래시 카드 */}
-                    <div className="border-4 border-yellow-500 w-full">
-                        플래시카드
-                        {currentWord && <Flashcard currentWord={currentWord} />}
-                    </div>
-                    {/* 유저 실시간 화면 */}
-                    <div>
-                        유저 실시간 화면
-                        {/* <UserView /> */}
-                        <MyCam categoryNumber={3} changeFinger={changeFinger} />
-                    </div>
-                    <div className="flex justify-between">
-                        <button className="ml-auto h-8 px-5 font-medium rounded-lg outline-none ring-2 ring-yellow-600 ring-inset text-gray-800 hover:text-yellow-600">
-                            학습 종료
-                        </button>
-                    </div>
-                    <div>${finger}</div>
-                </div>
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <h1 className="text-3xl font-bold underline">학습 시작 페이지</h1>
+      <div className="flex">
+        {/* 사이드메뉴 */}
+        <div className="w-[270px] h-[90vh] p-1 border-4 bg-yellow-100">
+          <SideMenu
+            selectedMain={selectedMain}
+            selectedSub={selectedSub}
+            wordList={wordList}
+            setCurrentWord={setCurrentWord}
+          />
+        </div>
+        {/* 학습 화면 */}
+        <div className="px-40 ml-2 w-full">
+          {/* 플래시 카드 */}
+          <div className="border-4 w-100 bg-yellow-200 shadow-md rounded-lg p-10">
+            플래시카드
+            {currentWord && <Flashcard currentWord={currentWord} />}
+          </div>
+          {/* 유저 실시간 화면 */}
+          <div className="border-4 w-full bg-gray-200 shadow-md rounded-lg p-10">
+            유저 실시간 화면
+            {/* <UserView /> */}
+            <MyCam categoryNumber={3} changeFinger={changeFinger} />
+          </div>
+          <div className="flex justify-between">
+            <button className="ml-auto h-8 px-5 font-medium rounded-lg outline-none ring-2 ring-yellow-600 ring-inset text-gray-800 hover:text-yellow-600">
+              학습 종료
+            </button>
+          </div>
+          <div>${finger}</div>
+        </div>
+      </div>
+    </Container>
+  );
 }
