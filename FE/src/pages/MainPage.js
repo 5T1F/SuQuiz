e7d2176be36ba10a1066bd39acdf6b7fc7fb390a
정혜진 <pado7sea@gmail.com>
2024-02-11 06:35:40 +0900

    Feat: 학습하기 등 CSS

--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,10 +1,8 @@
 import { useNavigate } from "react-router-dom";
-
 import Container from "../components/Container";
-
 import styles from "./MainPage.module.css";
-
 import backgroundVideo from "../assets/backgroundVideo.mp4";
+import { motion } from "framer-motion";
 
 export default function MainPage() {
   const navigate = useNavigate();
@@ -29,6 +27,15 @@ export default function MainPage() {
               학습 시작
             </button>
           </div>
+          {/* <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ staggerChildren: 0.4 }} // 자식 컴포넌트들이 0.2초 간격으로 나타남
+          >
+            <motion.div transition={{ staggerChildren: 0.4 }}>수퀴즈 소개 1</motion.div>
+            <motion.div transition={{ staggerChildren: 0.8 }}>수퀴즈 소개 2</motion.div>
+            <motion.div transition={{ staggerChildren: 0.4 }}>수퀴즈 소개 3</motion.div>
+          </motion.div> */}
         </div>
       </Container>
       <video autoPlay loop muted>
