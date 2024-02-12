3fd2d1dc0eb67e0b9a038237709a8d16895c3017
김현준 <denny10002@naver.com>
2024-02-13 00:16:40 +0900

    feat : CORS config

--- BE/src/main/java/com/example/entity/config/WebConfig.java
+++ BE/src/main/java/com/example/entity/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, https://suquiz.shop",
+                .allowedOrigins("http://localhost:3000, https://suquiz.shop", "https://suquiz.shop:5000",
                         "http://localhost:5000", "http://127.0.0.1:3000", "http://127.0.0.1:5000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
