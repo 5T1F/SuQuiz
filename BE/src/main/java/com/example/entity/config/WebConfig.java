8662933e91bb731f91b96a0bad61f6d9537a1903
김현준 <denny10002@naver.com>
2024-02-12 13:22:27 +0900

    feat : origco: cross origin change

--- BE/src/main/java/com/example/entity/config/WebConfig.java
+++ BE/src/main/java/com/example/entity/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000, https://suquiz.shop",
+                        "http://localhost:5000", "http://127.0.0.1:3000", "http://127.0.0.1:5000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
