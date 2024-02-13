a85d23f160491bb69973f2848988198a1a79d3f7
김현준 <denny10002@naver.com>
2024-02-13 10:31:07 +0900

    feat : 스웨거 CORS 해결

--- BE/src/main/java/com/example/entity/EntityApplication.java
+++ BE/src/main/java/com/example/entity/EntityApplication.java
@@ -1,10 +1,20 @@
 package com.example.entity;
 
 import com.example.entity.socialLogin.oauth.util.JwtProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://suquiz.shop/api", description = "Suquiz Shop Server"),
+				@Server(url = "http://i10b302.p.ssafy.io/api", description = "Another Server"),
+				@Server(url = "http://localhost:5000/api", description = "Develop Server")
+		}
+)
 
 @SpringBootApplication
 @EnableScheduling
